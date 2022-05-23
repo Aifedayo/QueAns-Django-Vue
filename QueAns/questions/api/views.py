978496d50d9df76ac4773a1ab18a24ca1dbf435d
akeem lagundoye <akeemifedayolag@gmail.com>
2022-05-23 21:12:01 +0100

    AnswerCreateAPIView

--- QueAns/questions/api/views.py
+++ QueAns/questions/api/views.py
@@ -1,9 +1,10 @@
 from questions.api.permissions import IsAuthorOrReadOnly
-from questions.api.serializers import QuestionSerializer
-from questions.models import Question
-from rest_framework import viewsets
+from questions.api.serializers import QuestionSerializer, AnswerSerializer
+from questions.models import Question, Answer
+from rest_framework.exceptions import ValidationError
+from rest_framework import generics, viewsets
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.generics import get_object_or_404
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all().order_by('-created_at')
@@ -13,3 +14,17 @@ class QuestionViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+# /question/slug/answer
+class AnswerCreateAPIView(generics.CreateAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        request_user = self.request.user
+        kwarg_slug = self.kwargs.get('slug')
+        question = get_object_or_404(Question, slug=kwarg_slug)
+        if question.answers.filter(author=request_user).exists():
+            raise ValidationError("You have already answered this Question!")
+        serializer.save(author=request_user, question=question)
