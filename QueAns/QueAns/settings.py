c354e7e8434ebaa821de5d7b69aa793a89a0e014
akeem lagundoye <akeemifedayolag@gmail.com>
2022-05-23 08:46:35 +0100

    authtoken and registration

--- QueAns/QueAns/settings.py
+++ QueAns/QueAns/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'rest_framework.authToken',
+    'rest_framework.authtoken',
 
     'django_registration',
     'djoser',
