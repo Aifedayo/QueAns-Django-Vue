06845089b86f51e9363bfaf992d516519f4533bb
akeem lagundoye <akeemifedayolag@gmail.com>
2022-05-28 14:02:25 +0100

    QuestionEditor

---
+++ QueAns/vue-frontend/src/views/QuestionEditor.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container my-2">
+        <h1 class="mb-3">
+            Ask a Question
+        </h1>
+        <form action="">
+            <textarea 
+                class="form-control"
+                placeholder="What do you have in mind?"
+                rows="5">
+            </textarea>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "QuestionEditor"
+}
+</script>
