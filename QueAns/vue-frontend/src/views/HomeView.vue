52e9535c6e2922b6b8f27c50878dd2c07cf24b69
akeem lagundoye <akeemifedayolag@gmail.com>
2022-05-28 08:42:03 +0100

    defining methods to get a list of questions

--- QueAns/vue-frontend/src/views/HomeView.vue
+++ QueAns/vue-frontend/src/views/HomeView.vue
@@ -1,13 +1,33 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <button @click="getQuestion">Load Questions</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import { axios } from "@/common/api.service.js";
 export default {
+  name: "HomeView",
 
+  data() {
+    return {
+      questions: []
+    }
+  },
+
+  methods: {
+    async getQuestion() {
+      let endpoint = '/api/v1/questions/';
+      try {
+        const response = await axios.get(endpoint);
+        console.log(response)
+      } catch (error) {
+        console.error(error.response);
+        alert(error.response.statusText);
+      }
+    }
+  }
 }
 </script>
