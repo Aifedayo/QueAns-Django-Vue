eac6c3ed6da81442ea3cf1bda92f1c71d0e5f7b3
akeem lagundoye <akeemifedayolag@gmail.com>
2022-05-28 08:12:24 +0100

    navbar component

---
+++ QueAns/vue-frontend/src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse ml-auto" id="navbarSupportedContent">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+  name: "NavbarComponent",
+};
+</script>
+
+<style>
+.my-navbar {
+  border-bottom: 1px solid #ddd;
+}
+
+.navbar-brand {
+  font-weight: bold;
+  font-size: 150%;
+}
+
+.navbar-brand:hover {
+  color: #dc3545 !important;
+}
+</style>
